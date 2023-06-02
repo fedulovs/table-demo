10f2fa66a8ae9c607cebd4063c6d3d26062c0b44
fedulovs <heaven45@mail.ru>
2023-06-02 23:58:59 +0300

    Add pagination

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,11 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { BasicTable } from './components/BasicTable'
 import { FilteringTable } from './components/FilteringTable'
+import { PaginationTable } from './components/PaginationTable'
 
 
 export default function Home() {
   return (
-    <FilteringTable />
+    <PaginationTable />
   )
 }
